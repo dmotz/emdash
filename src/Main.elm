a665bf0aeb848cde73c177c2813e2570531cf651
Dan Motzenbecker <dan@oxism.com>
2019-01-01 20:52:55 -0500

    many ui updates

--- src/Main.elm
+++ src/Main.elm
@@ -125,39 +125,71 @@ update message model =
 
 view : Model -> Html Msg
 view model =
-    div [ id "container" ]
+    div
+        [ id "container"
+        , class <|
+            if model.focusMode then
+                "focus-mode"
+
+            else
+                ""
+        ]
         [ div [ id "controls" ]
             [ input
-                [ id "search", placeholder "search", onInput OnFilter ]
-                []
-            , span [ onClick ShowRandom ]
-                [ text "⚂" ]
-            ]
-        , div [ id "sidebar" ]
-            [ div []
-                [ ul []
-                    (List.map renderEntry <|
-                        if List.isEmpty model.shownEntries then
-                            model.entries
-
-                        else
-                            model.shownEntries
-                    )
+                [ onInput OnFilter
+                , id "search"
+                , placeholder "search"
+                , autocomplete False
+                , spellcheck False
                 ]
+                []
+            , div [ id "tools" ]
+                [ span [ onClick ShowRandom ]
+                    [ text "⚂" ]
+                , select [ onInput FilterTitle ]
+                    (option
+                        [ value "*" ]
+                        [ text "(all titles)" ]
+                        :: map
+                            (\t -> option [ value t ] [ text t ])
+                            model.titles
+                    )
+                , span [] [ text "×" ]
+                ]
+            , input
+                [ type_ "checkbox"
+                , checked model.focusMode
+                , onCheck SetFocusMode
+                ]
+                []
             ]
-        , div [ id "viewer" ]
-            [ case model.currentEntry of
-                Just entry ->
-                    div []
-                        [ p [] [ text entry.text ]
-                        , div [ id "meta" ]
-                            [ div [ class "title" ] [ text entry.title ]
-                            , div [ class "author" ] [ text entry.author ]
-                            ]
-                        ]
+        , main_ []
+            [ div [ id "sidebar" ]
+                [ div []
+                    [ ul []
+                        (map renderEntry <|
+                            if List.isEmpty model.shownEntries then
+                                model.entries
 
-                Nothing ->
-                    h3 [] [ text "Select an entry" ]
+                            else
+                                model.shownEntries
+                        )
+                    ]
+                ]
+            , div [ id "viewer" ]
+                [ case model.currentEntry of
+                    Just entry ->
+                        div []
+                            [ p [] [ text entry.text ]
+                            , div [ id "meta" ]
+                                [ div [ onClick (FilterTitle entry.title), class "title" ] [ text entry.title ]
+                                , div [ class "author" ] [ text entry.author ]
+                                ]
+                            ]
+
+                    Nothing ->
+                        h3 [] [ text "Select an entry" ]
+                ]
             ]
         ]
 
@@ -169,14 +201,17 @@ charLimit =
 renderEntry : Entry -> Html Msg
 renderEntry entry =
     li [ onClick (ShowEntry entry) ]
-        [ blockquote []
-            [ text
-                (if String.length entry.text > charLimit then
-                    String.slice 0 charLimit entry.text ++ "…"
+        [ a []
+            --[ href <| "/entry/" ++ entry.id ]
+            [ blockquote []
+                [ text
+                    (if String.length entry.text > charLimit then
+                        String.slice 0 charLimit entry.text ++ "…"
 
-                 else
-                    entry.text
-                )
+                     else
+                        entry.text
+                    )
+                ]
+            , div [ class "title" ] [ text entry.title ]
             ]
-        , div [ class "title" ] [ text entry.title ]
         ]
