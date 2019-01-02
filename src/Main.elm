3e444589f1d1fa8f904f45a0a585ffed482da5e8
Dan Motzenbecker <dan@oxism.com>
2019-01-01 22:33:22 -0500

    moved view out of main

--- src/Main.elm
+++ src/Main.elm
@@ -1,15 +1,14 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing (..)
 import Http
 import List exposing (drop, filter, head, length, map, sort)
-import Model exposing (Entry, Model, initialModel)
+import Model exposing (Model, initialModel)
+import Msg exposing (..)
 import Parser
 import Random exposing (generate)
 import Set
+import View exposing (view)
 
 
 main : Program Int Model Msg
@@ -34,16 +33,6 @@ init flags =
         , expect = Http.expectString OnFetch
         }
     )
-
-
-type Msg
-    = OnFetch (Result Http.Error String)
-    | ShowEntry Entry
-    | ShowRandom
-    | ShowByIndex Int
-    | OnFilter String
-    | FilterTitle String
-    | SetFocusMode Bool
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -121,97 +110,3 @@ update message model =
 
         SetFocusMode active ->
             ( { model | focusMode = active }, Cmd.none )
-
-
-view : Model -> Html Msg
-view model =
-    div
-        [ id "container"
-        , class <|
-            if model.focusMode then
-                "focus-mode"
-
-            else
-                ""
-        ]
-        [ div [ id "controls" ]
-            [ input
-                [ onInput OnFilter
-                , id "search"
-                , placeholder "search"
-                , autocomplete False
-                , spellcheck False
-                ]
-                []
-            , div [ id "tools" ]
-                [ span [ onClick ShowRandom ]
-                    [ text "⚂" ]
-                , select [ onInput FilterTitle ]
-                    (option
-                        [ value "*" ]
-                        [ text "(all titles)" ]
-                        :: map
-                            (\t -> option [ value t ] [ text t ])
-                            model.titles
-                    )
-                , span [] [ text "×" ]
-                ]
-            , input
-                [ type_ "checkbox"
-                , checked model.focusMode
-                , onCheck SetFocusMode
-                ]
-                []
-            ]
-        , main_ []
-            [ div [ id "sidebar" ]
-                [ div []
-                    [ ul []
-                        (map renderEntry <|
-                            if List.isEmpty model.shownEntries then
-                                model.entries
-
-                            else
-                                model.shownEntries
-                        )
-                    ]
-                ]
-            , div [ id "viewer" ]
-                [ case model.currentEntry of
-                    Just entry ->
-                        div []
-                            [ p [] [ text entry.text ]
-                            , div [ id "meta" ]
-                                [ div [ onClick (FilterTitle entry.title), class "title" ] [ text entry.title ]
-                                , div [ class "author" ] [ text entry.author ]
-                                ]
-                            ]
-
-                    Nothing ->
-                        h3 [] [ text "Select an entry" ]
-                ]
-            ]
-        ]
-
-
-charLimit =
-    200
-
-
-renderEntry : Entry -> Html Msg
-renderEntry entry =
-    li [ onClick (ShowEntry entry) ]
-        [ a []
-            --[ href <| "/entry/" ++ entry.id ]
-            [ blockquote []
-                [ text
-                    (if String.length entry.text > charLimit then
-                        String.slice 0 charLimit entry.text ++ "…"
-
-                     else
-                        entry.text
-                    )
-                ]
-            , div [ class "title" ] [ text entry.title ]
-            ]
-        ]
