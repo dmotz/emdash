d46674019b84fa6604015ad3bbfb65691681d512
Dan Motzenbecker <dan@oxism.com>
2019-02-17 17:22:52 -0500

    handle malformed models on load

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ require('./styles.sass')
 const {Elm} = require('./Main')
 const lsNs = 'ls'
 const state = window.localStorage.getItem(lsNs)
-const app = Elm.Main.init({flags: state ? JSON.parse(state) : null})
+let app
+
+try {
+  app = Elm.Main.init({flags: state ? JSON.parse(state) : null})
+} catch (e) {
+  console.warn('Failed to handle persisted state:', e)
+  app = Elm.Main.init({flags: null})
+}
+
 app.ports.setStorage.subscribe(state =>
   window.localStorage.setItem(lsNs, JSON.stringify(state))
 )
