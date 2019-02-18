d2f99bdc13eb711883dba0b9c87db7adb8117168
Dan Motzenbecker <dan@oxism.com>
2019-02-17 19:41:26 -0500

    getIndex util

--- src/Utils.elm
+++ src/Utils.elm
@@ -1,4 +1,4 @@
-module Utils exposing (insertOnce)
+module Utils exposing (getIndex, insertOnce)
 
 import Set
 
@@ -6,3 +6,21 @@ import Set
 insertOnce : List comparable -> comparable -> List comparable
 insertOnce list x =
     Set.toList <| Set.insert x (Set.fromList list)
+
+
+getIndex : List a -> a -> Int
+getIndex list item =
+    let
+        f l target n =
+            case l of
+                [] ->
+                    -1
+
+                x :: xs ->
+                    if x == target then
+                        n
+
+                    else
+                        f xs target (n + 1)
+    in
+    f list item 0
