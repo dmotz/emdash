41628f91a8a3355cb107839e8fe49dc4cecdd99f
Dan Motzenbecker <dan@oxism.com>
2019-05-28 21:58:35 -0400

    intro view as separate constant

--- src/View.elm
+++ src/View.elm
@@ -369,57 +369,7 @@ viewer mEntry parsingError noEntries tags pendingTag =
                         p [ class "error" ] [ text "Error parsing file." ]
 
                       else if noEntries then
-                        div []
-                            [ p [ class "big" ]
-                                [ text <|
-                                    "This is Marginalia, a tool to "
-                                        ++ "organize excerpts from ebooks "
-                                        ++ "with tags, notes, and search."
-                                ]
-                            , h4 [] [ text "To begin:" ]
-                            , div [ id "instructions" ]
-                                [ p []
-                                    [ text <|
-                                        "Drop a clippings text file onto this "
-                                            ++ "page to import its excerpts."
-                                    ]
-                                , p []
-                                    [ text "Or, "
-                                    , a [ onClick PickFile ]
-                                        [ text "click here" ]
-                                    , text " to browse for the file."
-                                    ]
-                                ]
-                            , h4 [] [ em [] [ text "Nota bene:" ] ]
-                            , ol []
-                                [ li []
-                                    [ text <|
-                                        "Marginalia works entirely on your "
-                                            ++ "device and stores all your "
-                                            ++ "data there."
-                                    ]
-                                , li []
-                                    [ text <|
-                                        "You can easily export/import your "
-                                            ++ "data (tags, notes, &c.) as "
-                                    , span
-                                        [ class "small-caps" ]
-                                        [ text "json" ]
-                                    , text "."
-                                    ]
-                                , li []
-                                    [ text "It works offline." ]
-                                , li []
-                                    [ text "It’s "
-                                    , a [ href repoUrl, target "_blank" ]
-                                        [ text "open source" ]
-                                    , text "."
-                                    ]
-                                , li [] [ text "You might like it." ]
-                                ]
-                            , p [] [ text "❦" ]
-                            , footer [] [ text "Habent sua fata libelli" ]
-                            ]
+                        introView
 
                       else
                         text ""
@@ -603,6 +553,54 @@ selectMenu values mState inputFn default =
                             defaultLabel
                 ]
             ]
+        ]
+
+
+introView : Html Msg
+introView =
+    div []
+        [ p [ class "big" ]
+            [ text <|
+                "This is Marginalia, a tool to organize excerpts from ebooks "
+                    ++ "with tags, notes, and search."
+            ]
+        , h4 [] [ text "To begin:" ]
+        , div [ id "instructions" ]
+            [ p []
+                [ text <|
+                    "Drop a clippings text file onto this page to import "
+                        ++ "its excerpts."
+                ]
+            , p []
+                [ text "Or, "
+                , a [ onClick PickFile ] [ text "click here" ]
+                , text " to browse for the file."
+                ]
+            ]
+        , h4 [] [ em [] [ text "Nota bene:" ] ]
+        , ol []
+            [ li []
+                [ text <|
+                    "Marginalia works entirely on your device and stores all "
+                        ++ "your data there."
+                ]
+            , li []
+                [ text <|
+                    "You can easily export your data (tags, notes, &c.) as "
+                , span [ class "small-caps" ] [ text "json" ]
+                , text "."
+                ]
+            , li []
+                [ text "It works offline." ]
+            , li []
+                [ text "It’s "
+                , a [ href repoUrl, target "_blank" ] [ text "open source" ]
+                , text "."
+                ]
+            , li [] [ text "You might like it." ]
+            ]
+        , p [] [ text "❦" ]
+        , footer [] [ text "Habent sua fata libelli" ]
         ]
 
 
