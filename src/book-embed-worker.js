394448786db2d0e82f51fef22d54089e4c63399a
Dan Motzenbecker <dan@oxism.com>
2022-07-17 23:47:33 -0400

    handle empty books

--- src/book-embed-worker.js
+++ src/book-embed-worker.js
@@ -2,11 +2,13 @@ self.addEventListener('message', ({data}) =>
   self.postMessage({
     embeddingPairs: data.sets.map(([bookId, ids]) => [
       bookId,
-      ids
-        .map(id => data.embeddings[id])
-        .filter(x => x)
-        .reduce((a, c) => a.map((n, i) => n + c[i]))
-        .map(n => n / data.sets[0][1][0].length)
+      ids.length
+        ? ids
+            .map(id => data.embeddings[id])
+            .filter(x => x)
+            .reduce((a, c) => a.map((n, i) => n + c[i]))
+            .map(n => n / data.sets[0][1][0].length)
+        : []
     ])
   })
 )
