c3b6f881cf2add7a9516a6e8f3f0cdcfb1dadad1
Dan Motzenbecker <dan@oxism.com>
2022-08-14 13:41:34 -0400

    removed sorter from header

--- src/Views/Header.elm
+++ src/Views/Header.elm
@@ -29,24 +29,17 @@ import Html.Attributes
         , value
         )
 import Html.Events exposing (onBlur, onClick, onFocus, onInput)
-import List exposing (map)
-import Model
-    exposing
-        ( BookSort(..)
-        , Filter(..)
-        , InputFocus(..)
-        , sortToString
-        , stringToSort
-        )
+import Model exposing (BookSort(..), Filter(..), InputFocus(..))
 import Msg exposing (Msg(..))
 import Router exposing (authorToRoute)
 
 
-headerView : Maybe Filter -> String -> Bool -> Bool -> Html Msg
-headerView filter searchQuery reverseSort hideHeader =
+headerView : Maybe Filter -> String -> Bool -> Html Msg
+headerView filter searchQuery hideHeader =
     header
         [ classList [ ( "hidden", hideHeader ) ] ]
-        (a [ class "logo", href "/" ]
+        [ a
+            [ class "logo", href "/" ]
             [ img
                 [ src "/logo.svg"
                 , draggable "false"
@@ -54,75 +47,41 @@ headerView filter searchQuery reverseSort hideHeader =
                 ]
                 []
             ]
-            :: (case filter of
-                    Just (TitleFilter book) ->
-                        [ h1 []
-                            [ span [ class "title" ] [ text book.title ]
-                            , span [ class "divider" ] [ text "|" ]
-                            , a
-                                [ href <| authorToRoute book.author ]
-                                [ text book.author ]
-                            ]
+        , case filter of
+            Just (TitleFilter book) ->
+                h1 []
+                    [ span [ class "title" ] [ text book.title ]
+                    , span [ class "divider" ] [ text "|" ]
+                    , a
+                        [ href <| authorToRoute book.author ]
+                        [ text book.author ]
+                    ]
+
+            Just (AuthorFilter author) ->
+                h1 [] [ text author ]
+
+            _ ->
+                div
+                    [ class "search" ]
+                    [ input
+                        [ onInput OnSearch
+                        , onFocus <| SetInputFocus (Just SearchFocus)
+                        , onBlur <| SetInputFocus Nothing
+                        , spellcheck False
+                        , placeholder "search"
+                        , value searchQuery
                         ]
+                        []
+                    , button
+                        [ onClick <| OnSearch ""
+                        , class
+                            (if String.isEmpty searchQuery then
+                                ""
 
-                    _ ->
-                        [ div
-                            [ class "search" ]
-                            [ input
-                                [ onInput OnSearch
-                                , onFocus <| SetInputFocus (Just SearchFocus)
-                                , onBlur <| SetInputFocus Nothing
-                                , spellcheck False
-                                , placeholder "search"
-                                , value searchQuery
-                                ]
-                                []
-                            , button
-                                [ onClick <| OnSearch ""
-                                , class
-                                    (if String.isEmpty searchQuery then
-                                        ""
-
-                                     else
-                                        "active"
-                                    )
-                                ]
-                                [ text "✕" ]
-                            ]
-                        , div [ class "sorter" ]
-                            [ label []
-                                [ text "sort by: "
-                                , select [ onInput (stringToSort >> SortBooks) ]
-                                    (map
-                                        (\sort ->
-                                            let
-                                                s =
-                                                    sortToString sort
-                                            in
-                                            option [ value s ] [ text s ]
-                                        )
-                                        [ RecencySort, TitleSort, NumSort ]
-                                    )
-                                ]
-                            , button
-                                [ onClick Sort
-                                , title <|
-                                    "sort "
-                                        ++ (if reverseSort then
-                                                "descending"
-
-                                            else
-                                                "ascending"
-                                           )
-                                ]
-                                [ text <|
-                                    if reverseSort then
-                                        "▲"
-
-                                    else
-                                        "▼"
-                                ]
-                            ]
+                             else
+                                "active"
+                            )
                         ]
-               )
-        )
+                        [ text "✕" ]
+                    ]
+        ]
