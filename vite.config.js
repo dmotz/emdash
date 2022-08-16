4efd94760caa75733a4f47f1d5b06921b570344f
Dan Motzenbecker <dan@oxism.com>
2022-08-16 19:54:02 -0400

    updated build config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import elmPlugin from 'vite-plugin-elm'
 export default defineConfig({
   publicDir: 'assets',
   server: {port: 1999},
-  plugins: [elmPlugin({debug: false})]
+  plugins: [elmPlugin({debug: false})],
+  esbuild: {legalComments: 'none'}
 })
