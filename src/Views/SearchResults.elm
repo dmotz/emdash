ea6635f5aef4c3bcb6e144b6547b2bc1f305bcbe
Dan Motzenbecker <dan@oxism.com>
2022-09-25 18:46:31 -0400

    updated searchResults

--- src/Views/SearchResults.elm
+++ src/Views/SearchResults.elm
@@ -6,55 +6,52 @@ import Html.Attributes exposing (class)
 import Html.Lazy exposing (lazy4)
 import List exposing (filterMap, isEmpty, map)
 import Maybe exposing (andThen)
-import Model exposing (BookMap, EntryMap, Id)
+import Model exposing (Book, BookMap, BookSort(..), Entry, EntryMap, Id)
 import Msg exposing (Msg)
-import Utils exposing (pluckIds)
 import Views.BookList exposing (bookList)
 import Views.Snippet exposing (snippetView)
 
 
 searchResults :
     BookMap
-    -> List Id
     -> EntryMap
-    -> List Id
+    -> List Book
+    -> List Entry
     -> List ( Id, Float )
     -> String
     -> Html Msg
-searchResults books bookIds entries entryIds semanticMatches query =
+searchResults bookMap entryMap books entries semanticMatches query =
     div
         [ class "searchResults" ]
-        [ if isEmpty bookIds then
+        [ if isEmpty books && isEmpty entries then
+            p
+                [ class "noResults" ]
+                [ text "No results found." ]
+
+          else if isEmpty books then
             text ""
 
           else
-            bookList <| pluckIds books bookIds
-        , if isEmpty entryIds then
+            bookList books TitleSort False
+        , if isEmpty entries then
             text ""
 
           else
             ul
                 [ class "snippets" ]
                 (map
-                    (lazy4 snippetView books Nothing (Just query))
-                    (pluckIds entries entryIds)
+                    (lazy4 snippetView bookMap Nothing (Just query))
+                    entries
                     ++ map
                         (\( entry, score ) ->
-                            snippetView books (Just score) (Just query) entry
+                            snippetView bookMap (Just score) (Just query) entry
                         )
                         (filterMap
                             (\( id, score ) ->
-                                get id entries
+                                get id entryMap
                                     |> andThen (\entry -> Just ( entry, score ))
                             )
                             semanticMatches
                         )
                 )
-        , if isEmpty bookIds && isEmpty entryIds then
-            p
-                [ class "noResults" ]
-                [ text "No results found." ]
-
-          else
-            text ""
         ]
