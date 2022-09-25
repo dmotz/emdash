1027874819f3c093300e79e3dd707e8bf7a8e6c5
Dan Motzenbecker <dan@oxism.com>
2022-09-25 18:45:58 -0400

    updated entryList

--- src/Views/EntryList.elm
+++ src/Views/EntryList.elm
@@ -6,35 +6,35 @@ import Html.Attributes exposing (class)
 import Html.Keyed as Keyed
 import List exposing (indexedMap)
 import Maybe exposing (withDefault)
-import Model exposing (BookMap, EntryMap, EntryTab(..), Id, NeighborMap)
+import Model exposing (BookMap, Entry, EntryMap, EntryTab(..), Id, NeighborMap)
 import Msg exposing (Msg)
-import Utils exposing (pluckIds)
 import Views.Entry exposing (entryView)
 
 
 entryList :
-    List Id
+    List Entry
     -> EntryMap
     -> BookMap
     -> NeighborMap
     -> Dict Id Bool
     -> Dict Id EntryTab
     -> Html Msg
-entryList ids entries books neighbors idToShowDetails idToActiveTab =
+entryList entries entryMap books neighbors idToShowDetails idToActiveTab =
     Keyed.ul
         [ class "entries" ]
         (indexedMap
             (\i entry ->
                 ( entry.id
                 , entryView
-                    entries
+                    entryMap
                     books
                     neighbors
                     (withDefault False (get entry.id idToShowDetails))
                     (withDefault Related (get entry.id idToActiveTab))
                     i
+                    False
                     entry
                 )
             )
-            (pluckIds entries ids)
+            entries
         )
