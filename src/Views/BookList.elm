d8e478d599fc4337370b2dda93396c9b66cb01d4
Dan Motzenbecker <dan@oxism.com>
2022-10-23 13:47:48 -0400

    split book view from bookList

--- src/Views/BookList.elm
+++ src/Views/BookList.elm
@@ -1,10 +1,10 @@
-module Views.BookList exposing (bookList)
+module Views.BookList exposing (book, bookList)
 
 import Html exposing (Html, a, div, li, text)
 import Html.Attributes exposing (class, href)
 import Html.Keyed as Keyed
 import List exposing (map)
-import Model exposing (Book, BookSort)
+import Model exposing (Author, Book, BookSort, Title)
 import Msg exposing (Msg)
 import Router exposing (titleToRoute)
 import String exposing (fromInt)
@@ -16,17 +16,19 @@ bookList books sort reverseSort =
     Keyed.ul
         [ class "bookList" ]
         (map
-            (\{ id, title, author, count } ->
-                ( id
-                , li []
-                    [ a
-                        [ href <| titleToRoute title ]
-                        [ div [ class "title" ] [ text title ]
-                        , div [ class "author" ] [ text author ]
-                        , div [ class "count" ] [ text <| fromInt count ]
-                        ]
-                    ]
-                )
-            )
+            (\{ id, title, author, count } -> ( id, book title author count ))
             (sortBooks sort reverseSort books)
         )
+
+
+book : Title -> Author -> Int -> Html Msg
+book title author count =
+    li
+        [ class "book" ]
+        [ a
+            [ href <| titleToRoute title ]
+            [ div [ class "title" ] [ text title ]
+            , div [ class "author" ] [ text author ]
+            , div [ class "count" ] [ text <| fromInt count ]
+            ]
+        ]
