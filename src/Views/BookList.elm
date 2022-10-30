d071df6dc28e6caf6c652b07a28c76b6e87c5bc1
Dan Motzenbecker <dan@oxism.com>
2022-10-30 19:21:48 -0400

    show ratings in book list when rating sort

--- src/Views/BookList.elm
+++ src/Views/BookList.elm
@@ -4,38 +4,44 @@ import Html exposing (Html, a, div, li, text)
 import Html.Attributes exposing (class, href)
 import Html.Keyed as Keyed
 import List exposing (map)
-import Model exposing (Author, Book, BookSort, Title)
+import Model exposing (Book, BookSort(..))
 import Msg exposing (Msg)
 import Router exposing (titleToRoute)
 import String exposing (fromInt)
-import Utils exposing (sortBooks)
+import Utils exposing (null, ratingStr, sortBooks)
 
 
 bookList : List Book -> BookSort -> Bool -> Html Msg
 bookList books sort reverseSort =
+    let
+        showRating =
+            sort == RatingSort
+    in
     Keyed.ul
         [ class "bookList" ]
         (map
-            (\{ id, title, author, count } ->
-                ( id, bookView li title author count )
-            )
+            (\book -> ( book.id, bookView li book showRating ))
             (sortBooks sort reverseSort books)
         )
 
 
 bookView :
     (List (Html.Attribute msg) -> List (Html Msg) -> Html Msg)
-    -> Title
-    -> Author
-    -> Int
+    -> Book
+    -> Bool
     -> Html Msg
-bookView tag title author count =
+bookView tag book showRating =
     tag
         [ class "book" ]
         [ a
-            [ href <| titleToRoute title ]
-            [ div [ class "title" ] [ text title ]
-            , div [ class "author" ] [ text author ]
-            , div [ class "count" ] [ text <| fromInt count ]
+            [ href <| titleToRoute book.title ]
+            [ div [ class "title" ] [ text book.title ]
+            , div [ class "author" ] [ text book.author ]
+            , div [ class "count" ] [ text <| fromInt book.count ]
+            , if showRating then
+                div [ class "rating" ] [ text <| ratingStr book ]
+
+              else
+                null
             ]
         ]
