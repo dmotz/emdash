a15fcb2713da8ec1407fb71207cac091cd621442
Dan Motzenbecker <dan@oxism.com>
2022-10-30 22:57:05 -0400

    handle fragments in router

--- src/Router.elm
+++ src/Router.elm
@@ -14,14 +14,25 @@ import Dict exposing (get)
 import Model exposing (Author, BookMap, Entry, Id, Tag, Title)
 import Regex exposing (replace)
 import Url.Builder exposing (absolute)
-import Url.Parser exposing ((</>), (<?>), Parser, map, oneOf, s, string, top)
+import Url.Parser
+    exposing
+        ( (</>)
+        , (<?>)
+        , Parser
+        , fragment
+        , map
+        , oneOf
+        , s
+        , string
+        , top
+        )
 import Url.Parser.Query as Query
 import Utils exposing (rx)
 
 
 type Route
     = RootRoute
-    | TitleRoute Title
+    | TitleRoute Title (Maybe String)
     | EntryRoute Title Id
     | AuthorRoute Author
     | TagRoute Tag
@@ -33,7 +44,7 @@ routeParser : Parser (Route -> a) a
 routeParser =
     oneOf
         [ map RootRoute top
-        , map TitleRoute (s "title" </> string)
+        , map TitleRoute (s "title" </> string </> fragment identity)
         , map EntryRoute (s "title" </> string </> string)
         , map AuthorRoute (s "author" </> string)
         , map TagRoute (s "tag" </> string)
