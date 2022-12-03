02b7e9a6f88d8851059314117b583f2b14d0ec24
Dan Motzenbecker <dan@oxism.com>
2022-12-03 16:40:42 -0500

    zip worker

---
+++ src/zip-worker.js
@@ -0,0 +1,19 @@
+import JsZip from 'jszip'
+
+self.onmessage = ({data}) => {
+  const zip = new JsZip()
+  zip.folder('META-INF')
+  zip.folder('OEBPS')
+  data.forEach(([path, text]) => zip.file(path, text.trim()))
+
+  zip
+    .generateAsync({type: 'blob'})
+    .then(zipData =>
+      self.postMessage([
+        `marginalia_excerpts_${new Date().toLocaleDateString()}.epub`,
+        zipData
+      ])
+    )
+}
+
+export default {}
