a6eddd0a2a41c55d65ee190bbec368c12d8f07e7
Dan Motzenbecker <dan@oxism.com>
2023-01-02 10:24:13 -0500

    set mimetype file first in zip worker

--- src/zip-worker.js
+++ src/zip-worker.js
@@ -2,6 +2,7 @@ import JsZip from 'jszip'
 
 self.onmessage = ({data}) => {
   const zip = new JsZip()
+  zip.file('mimetype', 'application/epub+zip')
   zip.folder('META-INF')
   zip.folder('OEBPS')
   data.forEach(([path, text]) => zip.file(path, text.trim()))
