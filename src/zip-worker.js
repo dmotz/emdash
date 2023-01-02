dc35a71461535839d0e27b1f721d152689673ab8
Dan Motzenbecker <dan@oxism.com>
2023-01-02 10:24:29 -0500

    strip out invalid characters from epub in zip worker

--- src/zip-worker.js
+++ src/zip-worker.js
@@ -5,7 +5,15 @@ self.onmessage = ({data}) => {
   zip.file('mimetype', 'application/epub+zip')
   zip.folder('META-INF')
   zip.folder('OEBPS')
-  data.forEach(([path, text]) => zip.file(path, text.trim()))
+  data.forEach(([path, text]) =>
+    zip.file(
+      path,
+      text
+        .trim()
+        // eslint-disable-next-line
+        .replaceAll(/[^\u0009\u000a\u000d\u0020-\uD7FF\uE000-\uFFFD]/g, '')
+    )
+  )
 
   zip
     .generateAsync({type: 'blob'})
