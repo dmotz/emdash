69a71710f68810f0127ee608d57e275ff2aeaa4f
Dan Motzenbecker <dan@oxism.com>
2023-01-04 22:40:49 -0500

    overhauled search results view

--- src/Views/SearchResults.elm
+++ src/Views/SearchResults.elm
@@ -1,12 +1,22 @@
 module Views.SearchResults exposing (searchResults)
 
 import Dict exposing (get)
-import Html exposing (Html, div, h2, p, sup, text)
-import Html.Attributes exposing (class)
+import Html exposing (Html, button, div, li, p, sup, text, ul)
+import Html.Attributes exposing (class, classList)
+import Html.Events exposing (onClick)
 import Html.Keyed as Keyed
 import List exposing (filterMap, isEmpty, length, map, take)
-import Model exposing (Book, BookMap, BookSort(..), Entry, EntryMap, ScorePairs)
-import Msg exposing (Msg)
+import Model
+    exposing
+        ( Book
+        , BookMap
+        , BookSort(..)
+        , Entry
+        , EntryMap
+        , ScorePairs
+        , SearchMode(..)
+        )
+import Msg exposing (Msg(..))
 import Utils exposing (juxt, null)
 import Views.BookList exposing (bookList)
 import Views.Snippet exposing (snippetView)
@@ -18,91 +28,87 @@ maxResults =
 
 
 searchResults :
-    BookMap
+    SearchMode
+    -> BookMap
     -> EntryMap
     -> List Book
     -> List Entry
     -> ScorePairs
-    -> Bool
     -> String
     -> Html Msg
-searchResults bookMap entryMap books entries semanticMatches semanticReady query =
-    if isEmpty books && isEmpty entries && isEmpty semanticMatches && semanticReady then
-        p
-            [ class "noResults" ]
-            [ text "No results found." ]
-
-    else
-        div
-            [ class "searchResults" ]
-            [ if isEmpty books then
-                null
-
-              else
-                bookList books TitleSort False
-            , div
-                [ class "snippets" ]
-                ((if not (isEmpty entries) then
-                    [ resultsSection
-                        "Text matches"
-                        query
-                        bookMap
-                        (map (juxt identity (always Nothing)) entries)
-                    ]
-
-                  else
-                    []
-                 )
-                    ++ (if not (isEmpty semanticMatches) then
-                            [ resultsSection
-                                "Semantic matches"
-                                query
-                                bookMap
-                                (filterMap
-                                    (\( id, score ) ->
-                                        get id entryMap
-                                            |> Maybe.map
-                                                (\entry ->
-                                                    ( entry, Just score )
-                                                )
-                                    )
-                                    semanticMatches
-                                )
-                            ]
-
-                        else
-                            []
-                       )
-                )
-            ]
-
-
-resultsSection : String -> String -> BookMap -> List ( Entry, Maybe Float ) -> Html Msg
-resultsSection title query bookMap items =
+searchResults mode bookMap entryMap books matches semanticMatches query =
     let
-        len =
-            length items
+        textMatches =
+            map (juxt identity (always Nothing)) matches
+
+        semMatches =
+            filterMap
+                (\( id, score ) ->
+                    get id entryMap
+                        |> Maybe.map
+                            (\entry ->
+                                ( entry, Just score )
+                            )
+                )
+                semanticMatches
+
+        ( list, label ) =
+            if mode == TextMatches then
+                ( textMatches, "text matches" )
+
+            else
+                ( semMatches, "semantic matches" )
     in
     div
-        []
-        [ h2
-            []
-            [ text title
-            , sup []
-                [ text <|
-                    if len > maxResults then
-                        ">" ++ String.fromInt maxResults
+        [ class "searchResults" ]
+        [ if isEmpty books then
+            null
 
-                    else
-                        String.fromInt len
+          else
+            bookList books TitleSort False
+        , div
+            [ class "snippets" ]
+            [ div
+                [ class "modeHeading" ]
+                [ ul
+                    []
+                    (map
+                        (\( m, title, len ) ->
+                            li
+                                [ classList [ ( "active", m == mode ) ]
+                                , onClick (SetSearchTab m)
+                                ]
+                                [ button [] [ text title ]
+                                , sup []
+                                    [ text <|
+                                        if len > maxResults then
+                                            ">" ++ String.fromInt maxResults
+
+                                        else
+                                            String.fromInt len
+                                    ]
+                                ]
+                        )
+                        [ ( TextMatches, "Text matches", length textMatches )
+                        , ( SemanticMatches, "Semantic matches", length semMatches )
+                        ]
+                    )
                 ]
+            , if isEmpty list then
+                p
+                    [ class "noResults" ]
+                    [ text <| "No " ++ label ++ " found." ]
+
+              else
+                Keyed.ul
+                    []
+                    (map
+                        (\( entry, mScore ) ->
+                            ( entry.id
+                            , snippetView bookMap mScore (Just query) entry
+                            )
+                        )
+                        (take maxResults list)
+                    )
             ]
-        , Keyed.ul
-            []
-            (map
-                (\( entry, mScore ) ->
-                    ( entry.id, snippetView bookMap mScore (Just query) entry )
-                )
-                (take maxResults items)
-            )
         ]
