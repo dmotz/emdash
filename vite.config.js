5fb0f06ceae37e4d70822041408f864066de7eac
Dan Motzenbecker <dan@oxism.com>
2023-04-24 22:17:03 -0400

    minify html

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import {defineConfig} from 'vite'
 import elmPlugin from 'vite-plugin-elm'
+import {ViteMinifyPlugin} from 'vite-plugin-minify'
 
 export default defineConfig({
   publicDir: 'assets',
   server: {port: 1999},
-  plugins: [elmPlugin({debug: false})],
+  plugins: [elmPlugin({debug: false}), ViteMinifyPlugin({})],
   esbuild: {legalComments: 'none'}
 })
